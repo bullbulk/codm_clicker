5721a56f73e674a1fe3275e92a2c305b965de4af
bullbulk <grur.ura@mail.ru>
2020-10-25 03:29:46 +0500

    setup changes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-apt update && apt install wget && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
+apt update
+apt install wget
+wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh
+bash InstallTools.sh
 pkg install python
-pkg install tesseract
 pip install pytesseract
+apt install tesseract
