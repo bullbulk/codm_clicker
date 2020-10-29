4986390cfa86624e05bf96db95c046114d53dc1d
bullbulk <55855557+bullbulk@users.noreply.github.com>
2020-10-29 23:00:57 +0500

    Add files via upload

---
+++ notification_service.py
@@ -0,0 +1,50 @@
+import os
+import signal
+
+RUNNING = False
+
+
+def handler(signal, frame):
+    global RUNNING
+    if RUNNING:
+        os.system('termux-notification -t "Кликер остановлен"'
+                  ' -c "Нажмите кнопку уменьшения громкости 2 раза, чтобы остановить" '
+                  '--priority max --id "clicker" --vibrate 100 --ongoing')
+        RUNNING = False
+    else:
+        os.system('termux-notification -t "Кликер запущен" '
+                  '-c "Нажмите кнопку уменьшения громкости 2 раза, чтобы остановить" '
+                  '--priority max --id "clicker" --vibrate 100 --ongoing')
+        RUNNING = True
+
+def main():
+    signal.signal(signal.SIGUSR1, handler)
+
+    pid = int(open('data/listener_pid', 'r').read())
+    os.system('echo 13 > a.txt')
+
+    with open('data/ntf_pid', 'w') as f:
+        f.write(str(os.getpid()))
+
+    pid_using = True
+
+    try:
+        os.kill(pid, 0)
+    except OSError:
+        pid_using = False
+    else:
+        pid_using = True
+
+    while pid_using:
+        try:
+            os.kill(pid, 0)
+        except OSError:
+            pid_using = False
+        else:
+            pid_using = True
+
+    else:
+        os.system('termux-notification-remove "clicker"')
+
+if __name__ == '__main__':
+    main()
