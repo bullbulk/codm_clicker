c9776f80b929a1c1dae44f2f50125cb83f8600dc
bullbulk <55855557+bullbulk@users.noreply.github.com>
2020-10-31 04:37:16 +0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 @echo off
 cd "$HOME" || exit
 apt -y install wget python tesseract git
+termux-setup-storage
 wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh
 bash "$HOME/InstallTools.sh"
 git clone https://github.com/bullbulk/codm_clicker.git
@@ -11,6 +12,5 @@ echo "cd $HOME/codm_clicker" > clicker.sh
 echo "python main.py" >> clicker.sh
 echo "alias clicker='bash $HOME/clicker.sh'" >> "$HOME/../usr/etc/bash.bashrc"
 echo "alias clicker-update='cd $HOME/codm_clicker && git pull'" >> "$HOME/../usr/etc/bash.bashrc"
-termux-setup-storage
 mkdir -p "/sdcard/codm_clicker"
 rm "$HOME/setup.sh"
