57348324835256abc11682a68e66251388a5e0e2
bullbulk <55855557+bullbulk@users.noreply.github.com>
2020-10-31 02:37:12 +0500

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,13 +1,10 @@
 cd $HOME || exit
-
 apt update && apt install wget && wget https://github.com/MasterDevX/Termux-ADB/raw/master/InstallTools.sh && bash InstallTools.sh
 pkg install python
 pkg install tesseract
-
 git clone https://github.com/bullbulk/codm_clicker.git
 cd codm_clicker || exit
 pip install -r requirements.txt
-
 echo "cd $HOME/codm_clicker" > clicker.sh
 echo "python main.py" >> clicker.sh
 echo alias clicker="bash $HOME/clicker.sh"
